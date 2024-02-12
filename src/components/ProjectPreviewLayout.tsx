49d3ef13c36a76f5d00f2bfa639169d09b517b33
MarkoTeixido <teixido_marko@outlook.es>
2024-02-11 23:46:18 -0300

    v1 - add projects preview photos

--- src/components/ProjectPreviewLayout.tsx
+++ src/components/ProjectPreviewLayout.tsx
@@ -1,13 +1,32 @@
+import React, { useState, useEffect } from "react";
 import ProjectPreviewCard from "../components/ProjectPreviewCard";
 
 const ProjectPreviewLayout = () => {
+    const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 768);
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const device = isSmallScreen ? 'M' : 'D';
+
     return (
-        <section id="projects" className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+        <section id="projects" className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4 te">
             <ProjectPreviewCard
                 name="Funkoshop - Ecommerce"
                 description="Fullstack project - NodeJS, ExpressJS and MySQL"
                 urlProject="https://github.com/MarkoTeixido/FunkoshopCaC-Backend"
-                imageUrl="/ProjectsPics/shotpic3.png"
+                imageUrl={`/ProjectsPics/shotpic${device}.png`}
                 bgColor="#685cdd"
                 dark
             />
@@ -15,7 +34,7 @@ const ProjectPreviewLayout = () => {
                 name="Shopi - Ecommerce"
                 description="React project with ViteJS and tailwind"
                 urlProject="https://github.com/MarkoTeixido/react-shopi"
-                imageUrl="/ProjectsPics/shot2pic3.png"
+                imageUrl={`/ProjectsPics/shot2pic${device}.png`}
                 bgColor="#313442"
                 dark
             />
@@ -23,12 +42,13 @@ const ProjectPreviewLayout = () => {
                 name="MarkoToDo - ToDo App"
                 description="React project with tailwind"
                 urlProject="https://github.com/MarkoTeixido/react-todo"
-                imageUrl="/ProjectsPics/shot3pic1.png"
+                imageUrl={`/ProjectsPics/shot3pic${device}.png`}
             />
             <ProjectPreviewCard
                 name="MM landing - Landing Page"
                 description="Landing page with api fetch"
                 urlProject="https://github.com/MarkoTeixido/landing_MM"
+                imageUrl={`/ProjectsPics/shot4pic${device}.png`}
                 bgColor="#4e4e56"
                 dark
             />
