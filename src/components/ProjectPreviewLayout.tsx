f75759e8256f442e7d5f9f3e57e52282cf3031e3
MarkoTeixido <teixido_marko@outlook.es>
2024-02-13 18:33:42 -0300

    v1 - language details

--- src/components/ProjectPreviewLayout.tsx
+++ src/components/ProjectPreviewLayout.tsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
+import { AppContext } from "../context/AppContext";
 import ProjectPreviewCard from "../components/ProjectPreviewCard";
 
 const ProjectPreviewLayout = () => {
+    //context
+    const context = useContext(AppContext);
     const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
 
     const handleResize = useCallback(() => {
@@ -17,35 +20,33 @@ const ProjectPreviewLayout = () => {
         };
     }, [handleResize]);
 
-    const device = isSmallScreen ? 'M' : 'D';
-
     return (
         <section id="projects" className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
             <ProjectPreviewCard
-                name="Funkoshop - Ecommerce"
-                description="Fullstack project - NodeJS, ExpressJS and MySQL"
+                name={context.content.projectPreviewLayout.project1.title}
+                description={context.content.projectPreviewLayout.project1.description}
                 urlProject="https://github.com/MarkoTeixido/FunkoshopCaC-Backend"
                 imageUrl={isSmallScreen ? 'https://i.imgur.com/Jy0Ua2Z.png' : 'https://i.imgur.com/6BEVHYE.png'}
                 bgColor="#685cdd"
                 dark
             />
             <ProjectPreviewCard
-                name="Shopi - Ecommerce"
-                description="React project with ViteJS and Tailwind"
+                name={context.content.projectPreviewLayout.project2.title}
+                description={context.content.projectPreviewLayout.project2.description}
                 urlProject="https://github.com/MarkoTeixido/react-shopi"
                 imageUrl={isSmallScreen ? 'https://i.imgur.com/cKWqcom.png' : 'https://i.imgur.com/cbV1ooh.png'}
                 bgColor="#313442"
                 dark
             />
             <ProjectPreviewCard
-                name="MarkoToDo - ToDo App"
-                description="React project with Tailwind"
+                name={context.content.projectPreviewLayout.project3.title}
+                description={context.content.projectPreviewLayout.project3.description}
                 urlProject="https://github.com/MarkoTeixido/react-todo"
                 imageUrl={isSmallScreen ? 'https://i.imgur.com/hCIp6wz.png' : 'https://i.imgur.com/sPpafvb.png'}
             />
             <ProjectPreviewCard
-                name="MM landing - Landing Page"
-                description="Landing page with api fetch"
+                name={context.content.projectPreviewLayout.project4.title}
+                description={context.content.projectPreviewLayout.project4.description}
                 urlProject="https://github.com/MarkoTeixido/landing_MM"
                 imageUrl={isSmallScreen ? 'https://i.imgur.com/wO7okdu.png' : 'https://i.imgur.com/AM4x7xU.png'}
                 bgColor="#4e4e56"
